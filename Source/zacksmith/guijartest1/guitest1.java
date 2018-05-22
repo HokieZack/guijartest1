259035e224d7366ec78f8d3fe0cd572bdc63f87a
HokieZack <39137775+HokieZack@users.noreply.github.com>
2018-05-22 18:51:21 -0400

    Update guitest1.java

--- Source/zacksmith/guijartest1/guitest1.java
+++ Source/zacksmith/guijartest1/guitest1.java
@@ -1,5 +1,20 @@
 package zacksmith.guijartest1;
 
+import javax.swing.*;
+
 public class guitest1 {
 
+	public static void main (String[] args) {
+		
+		JFrame frame = new JFrame();
+		JButton button = new JButton();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(button);
+		
+		frame.setSize(300, 300);
+		
+		frame.setVisible(true);
+	}
 }
